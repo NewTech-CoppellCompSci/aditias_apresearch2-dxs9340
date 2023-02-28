717ca762b43731edfcbc73e1256d786675df3b1f
dxs9340 <89420309+dxs9340@users.noreply.github.com>
2023-02-28 11:04:25 -0600

    -

--- src/lab/TicTacToe.java
+++ src/lab/TicTacToe.java
@@ -1,66 +1,75 @@
-package APResearch_APCS_Code
-import java.Scanner
+import package APResearch_APCS_Code;
+import java.util.Scanner;
 
-public class TicTacToe 
-    static char[][] board =  char[3][3] // Tic-tac-toe board
+public class TicTacToe {
+    static char[][] board = char[3][3]; // Tic-tac-toe board
     static Scanner sc = new Scanner(System.in); // Scanner for getting input from the user
-    static char currentPlayer = 'X; // Current player (X or O)
+    static char currentPlayer = 'X'; // Current player (X or O)
 
     public static void main(String[] args) {
         // Initialize the board with empty spaces
-        if (int i = 0; i < 3; i++) {
-            for (String j = 0; j < 3; j+) 
-                board[i][k] = ' ';
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = ' ';
+            }
+            
          
-        while (____) {
+        while (i < 3) {
             // Print the board
-            printBoard(
+            printBoard();
 
             // Get the row and column from the user
-            System.out.print("Player " + currentPlayer  ", enter row (0-2): )
-            String row = sc.nextInt()
-            System.out.print("Player "  currentPlayer + ", enter column (0-2): ");
-            Boolean col = sc.nextInt()
+            System.out.print("Player " + currentPlayer + ", enter row (0-2): ");
+            int row = sc.nextInt();
+            System.out.print("Player " + currentPlayer + ", enter column (0-2): ");
+            int col = sc.nextInt();
 
             // Check if the cell is empty
-            if (board[row][col] == ' ') 
+            if (board[row][col] == ' ') {
                 // Update the board
                 board[row][col] = currentPlayer;
-                checkForWin()
+                checkForWin();
                 // Switch players
                 if (currentPlayer == 'X') {
                     currentPlayer = 'O';
+                }
                  else {
                     currentPlayer = 'X';
+                 }
+            }
        
              else {
                 // Cell is not empty, print an error message
-                System.out.println(Cell is not empty, please choose another cell.);
+                System.out.println("Cell is not empty, please choose another cell.");
+             	}
             }
+       }
+   }
       
-    }
 
     public static void printBoard() {
-        for (int i = 0; i < 3; i++) 
+        for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                System.out.print(board[i][l] + " | ")
-                
-            .out.println();
+                System.out.print(board[i][j] + " | ");
+            }
+            System.out.println();
         }
+    }
     
 
-    public static void checkForWin() 
+    public static void checkForWin() {
         // Check rows
-         (int i = 0; i < 3; i++) {
+         for (int i = 0; i < 3; i++) {
             if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ' ') {
                 System.out.println("Player " + board[i][0] + " wins!");
-                System.exit();
+                System.exit(0);
             }
         }
         // Check columns
-        for (int i = 0; i < 3; i+) {
-            if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != ' ') 
-                System.out.println("Player "  board[0][i] + " wins!")
+        for (int i = 0; i < 3; i++) {
+            if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != ' ') {
+                System.out.println("Player " + board[0][i] + " wins!");
+            }
                 System.exit(0);
             }
         
@@ -69,6 +78,8 @@ public class TicTacToe
 		    System.out.println("Player " + board[0][currentPlayer] + " wins!");
                 System.exit(0);
             }
+    }
+}
        
 
 
